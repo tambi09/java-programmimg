6d149fdbb55fdd718f6fb48804e9bf792b88b937
tambi09 <ritylya09@gmail.com>
2021-06-01 21:12:58 -0400

    static method added static

--- src/day49_static/StaticVariables.java
+++ src/day49_static/StaticVariables.java
@@ -2,4 +2,11 @@ package day49_static;
 
 public class StaticVariables {
 
+    static String name ="Cybertek";
+    static String userName = "admin@cybertek.com";
+    public static int age = 44;
+    public static double price = 5.99;
+
+
+
 }
