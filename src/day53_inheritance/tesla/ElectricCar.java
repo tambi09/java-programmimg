ae7d92b6bff01a2551448e4fb2b8e581f239b013
tambi09 <ritylya09@gmail.com>
2021-06-13 10:33:42 -0400

    added Electric car package

---
+++ src/day53_inheritance/tesla/ElectricCar.java
@@ -0,0 +1,41 @@
+package day53_inheritance.tesla;
+
+public class ElectricCar {
+    private String make;
+    private  String model;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    private double price;
+    private int year;
+}
