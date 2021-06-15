0c98c9b9a3c649557c18facd1e6d798da54d12c3
tambi09 <ritylya09@gmail.com>
2021-06-15 19:36:48 -0400

    added day 53 but not finished

---
+++ src/day53_inheritance/tesla/CarObjects.java
@@ -0,0 +1,8 @@
+package day53_inheritance.tesla;
+
+public class CarObjects {
+    public static void main(String[] args) {
+        ElectricCar ec1 = new ElectricCar("Tesla","Model Y", 48190.0,2021, 326 );
+
+    }
+}
--- src/day53_inheritance/tesla/ElectricCar.java
+++ src/day53_inheritance/tesla/ElectricCar.java
@@ -3,6 +3,42 @@ package day53_inheritance.tesla;
 public class ElectricCar {
     private String make;
     private  String model;
+    private double price;
+    private int year;
+    private int range;
+    private static int count;//all object wil share this variable
+    public static final int MAX_RANGE = 400;
+
+    public ElectricCar(String make, String model, double price, int year, int range) {
+
+        setMake(make);
+
+        count++;
+    }
+    public final void charge() {
+        System.out.println("Charging the Electric car using plug-in");
+        this.range = MAX_RANGE;
+    }
+
+    public static int getCount(){
+        return count;
+
+    }
+
+    @Override
+    public String toString() {
+        return "ElectricCar{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                ", range=" + range +
+                '}';
+    }
+
+
+
+
 
     public String getMake() {
         return make;
@@ -10,6 +46,11 @@ public class ElectricCar {
 
     public void setMake(String make) {
         this.make = make;
+        if(make.isEmpty()){
+            System.out.println("ERROR: Make connot be blank");
+        }else {
+            this.make = make;
+        }
     }
 
     public String getModel() {
@@ -36,6 +77,13 @@ public class ElectricCar {
         this.year = year;
     }
 
-    private double price;
-    private int year;
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
 }
+
+
