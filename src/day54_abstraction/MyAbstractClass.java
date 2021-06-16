c9d947c37f011053a13f383955c700b77a221e4d
tambi09 <ritylya09@gmail.com>
2021-06-15 21:25:47 -0400

    added day 45 missing classes

--- src/day54_abstraction/MyAbstractClass.java
+++ src/day54_abstraction/MyAbstractClass.java
@@ -1,5 +1,33 @@
 package day54_abstraction;
 
-public class MyAbstractClass {
+public abstract  class MyAbstractClass {
+    int num = 55;
+    public void learn(){
+        System.out.println("Learning....");
+    }
 
+    public abstract void close();
+
+}
+
+class Sub extends MyAbstractClass{
+    @Override
+    public void close(){
+        System.out.println("Close-abstract method implementation");
+    }
+
+
+        }
+
+class MyObjects{
+    public static void main(String[]args){
+       // MyAbstractClass mac = new MyAbstractClass();
+        Sub sub = new Sub();
+
+        sub.learn();
+        System.out.println(sub.num);
+        sub.close();
+
+
+    }
 }
