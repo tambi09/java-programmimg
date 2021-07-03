fbece28bbd4faa24374b70db737eb511e6f39d20
tambi09 <ritylya09@gmail.com>
2021-07-03 12:35:15 -0400

    added day 59

---
+++ src/day59_polymorphism_exceptions/ElementTest.java
@@ -0,0 +1,23 @@
+package day59_polymorphism_exceptions;
+
+public class ElementTest {
+    public static void main(String[] args) {
+        Link link= new Link();
+        link.click();
+        link.getText();
+        link.sendKeys("hi");
+        link.getLinkHref();
+
+        WebElement loginLink = new Link();
+        loginLink.click();
+        String txt = loginLink.getText();
+        System.out.println("txt=" + txt);
+
+        WebElement username = new InputField();
+        username.sendKeys("wooden spoon");
+        username.click();
+
+    }
+
+
+}
